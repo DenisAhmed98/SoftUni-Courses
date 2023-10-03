6907b941335137aa4e8fc6e032fb0b93af1c21de
Denis <denis.ahmed98@gmail.com>
2023-10-03 17:43:25 +0300

    Easter_Gifts update

--- Programming_Fundamentals_Python_2023/Lists Basics/Exercise/Easter_Gifts.py
+++ Programming_Fundamentals_Python_2023/Lists Basics/Exercise/Easter_Gifts.py
@@ -1,29 +1,77 @@
 items = [x for x in input().split()]
-values_input = input().split()
+items_first_iter = []
+items_second_iter = []
+items_third_iter = []
 outofstocklist = []
 required = []
 justincase = []
 
-if values_input[0] == "OutOfStock":
-    outofstocklist.append(values_input[1])
-elif values_input[0] == "Required":
-    required.append(values_input[1])
-    required.append(values_input[2])
-elif values_input[0] == "JustInCase":
-    justincase.append(values_input[1])
+command = input()
+for item in items:
+    if command == "No Money":
+        break
+    for item in items:
+        if "OutOfStock" in command:
+            if item == command.replace("OutOfStock ",""):
+                items_first_iter.append("None")
+        elif "Required" in command:
+            temp_commands = command.split()
+            if int(temp_commands[2]) > len(items):
+                break
+            else:
+                items_first_iter.pop(int(temp_commands[2]))
+                items_first_iter.insert(int(temp_commands[2]),temp_commands[1])
+                break
+        elif "JustInCase" in command:
+            justincase.append(command.replace("JustInCase ",""))
+        else:
+            items_first_iter.append(item)
 
-while values_input != "No Money":
-    if values_input[0] == "OutOfStock":
-        outofstocklist.append(values_input[1])
-    elif values_input[0] == "Required":
-        required.append(values_input[1])
-        required.append(values_input[2])
-    elif values_input[0] == "JustInCase":
-        justincase.append(values_input[1])
+# if "OutOfStock" in command:
+#     outofstocklist.append(command.replace("OutOfStock ",""))
+# elif "Required" in command:
+#     temp_commands = command.split()
+#     required.append(temp_commands[1])
+#     required.append(temp_commands[2])
+# elif "JustInCase" in command:
+#     justincase.append(command.replace("JustInCase ",""))
 
-counter = 0
-for x in items:
-    if counter ==
+while command != "No Money":
+    command = input()
+    if command == "No Money":
+        break
+    for item in items:
+        if "OutOfStock" in command:
+            if item == command.replace("OutOfStock ",""):
+                items_first_iter.append("None")
+        elif "Required" in command:
+            temp_commands = command.split()
+            if int(temp_commands[2]) > len(items):
+                break
+            else:
+                items_first_iter.pop(int(temp_commands[2]))
+                items_first_iter.insert(int(temp_commands[2]),temp_commands[1])
+                break
+        elif "JustInCase" in command:
+            justincase.append(command.replace("JustInCase ",""))
+        else:
+            items_first_iter.append(item)
+
+items_first_iter.pop(-1)
+items_first_iter.append(justincase[0])
+
+for x in items_first_iter:
+    if x == "None":
+        items_first_iter.remove("None")
 
 
-print(items)
+joined_string = " ".join(map(str,items_first_iter))
+print(joined_string, end="")
+
+
+
+
+
+
+
+#print(items)
