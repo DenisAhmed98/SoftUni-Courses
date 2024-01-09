e4182a6c4a74c3289301256051d38b1e8ec09fda
Denis <denis.ahmed98@gmail.com>
2024-01-09 22:39:14 +0200

    Create Matching_Parentheses.py

---
+++ Python Advanced 2024/Lists as Stacks and Queues/LAB/Matching_Parentheses.py
@@ -0,0 +1,10 @@
+text = input()
+parentheses = []
+for i in range(len(text)):
+    if text[i] == "(":
+        parentheses.append(i)
+    elif text[i] == ")":
+        start_index = parentheses.pop()
+        print(text[start_index:i+1])
+
+# from start_index to i element
