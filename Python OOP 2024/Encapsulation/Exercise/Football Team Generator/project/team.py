c2457fc5336dc25a7e84fc5ce58f9918e8a2402a
Denis <denis.ahmed98@gmail.com>
2024-03-18 20:44:18 +0200

    Python OOP: Encapsulation

---
+++ Python OOP 2024/Encapsulation/Exercise/Football Team Generator/project/team.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from project.player import Player
+
+
+class Team:
+    def __init__(self, name: str, rating: int):
+        self.__name = name
+        self.__rating = rating
+        self.__players: list[Player] = []
+
+    def add_player(self, player: Player) -> str:
+        if player in self.__players:
+            return f"Player {player.name} has already joined"
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name: str) -> Union[Player, str]:
+        try:
+            player = [p for p in self.__players if p.name == player_name][0]
+            self.__players.remove(player)
+            return player
+        except IndexError:
+            return f"Player {player_name} not found"
