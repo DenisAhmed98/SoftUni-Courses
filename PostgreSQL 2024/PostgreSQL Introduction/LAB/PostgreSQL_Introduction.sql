a98ab35cbbc2b9af483fa999f6321c53c8de9538
Denis <denis.ahmed98@gmail.com>
2024-05-14 21:42:28 +0300

    PostgreSQL Introduction LAB tasks

---
+++ PostgreSQL 2024/PostgreSQL Introduction/LAB/PostgreSQL_Introduction.sql
@@ -0,0 +1,40 @@
+CREATE TABLE employees
+(
+	id serial PRIMARY KEY NOT NULL,
+	first_name VARCHAR(30),
+	last_name VARCHAR(50),
+	hiring_date DATE DEFAULT '2023-01-01',
+	salary numeric(10,2),
+	devices_number integer
+);
+
+CREATE TABLE departments
+(
+	id serial PRIMARY KEY NOT NULL,
+	name VARCHAR(50),
+	code CHAR(3),
+	description text 
+);
+
+CREATE TABLE issues
+(
+	id serial PRIMARY KEY UNIQUE,
+	description VARCHAR(150),
+	date DATE,
+	start TIMESTAMPTZ
+);
+
+ALTER TABLE employees
+ADD COLUMN middle_name VARCHAR(50)
+
+ALTER TABLE employees
+ALTER COLUMN salary SET NOT NULL,
+ALTER COLUMN salary SET DEFAULT 0,
+ALTER COLUMN hiring_date SET NOT NULL
+
+ALTER TABLE employees
+ALTER COLUMN middle_name TYPE VARCHAR(100)
+
+TRUNCATE TABLE issues
+
+DROP TABLE departments
