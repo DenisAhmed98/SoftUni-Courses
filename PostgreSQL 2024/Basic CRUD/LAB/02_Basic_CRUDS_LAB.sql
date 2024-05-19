c548dfc5e4438813e66f499595d31e5756629bcf
Denis <denis.ahmed98@gmail.com>
2024-05-19 19:08:28 +0300

    Basic CRUDS Lab exercises

---
+++ PostgreSQL 2024/Basic CRUD/LAB/02_Basic_CRUDS_LAB.sql
@@ -0,0 +1,38 @@
+SELECT id,
+ first_name || ' ' || last_name
+ AS "Full Name",
+ job_title AS "Job Title"
+FROM employees;
+
+SELECT id,
+ first_name || ' ' || last_name
+ AS "full_Name",
+ job_title, salary
+FROM employees
+WHERE salary > 1000.00;
+
+SELECT id, first_name, last_name, job_title, department_id, salary
+FROM employees
+WHERE salary > 1000.00 AND department_id = 4
+ORDER BY id;
+
+INSERT INTO employees (first_name, last_name, job_title, department_id, salary)
+VALUES
+('Samantha','Young', 'Housekeeping', 4, 900),
+('Roger', 'Palmer', 'Waiter', 3, 928.33);
+SELECT * FROM employees;
+
+UPDATE employees
+ SET salary = salary + 100
+WHERE job_title  = 'Manager';
+SELECT * FROM employees
+WHERE job_title  = 'Manager';
+
+DELETE FROM employees
+WHERE department_id = 1 OR department_id = 2;
+SELECT * FROM employees
+ORDER BY id;
+
+CREATE VIEW top_paid_employee AS SELECT * FROM employees
+ORDER BY salary DESC LIMIT 1;
+SELECT * FROM top_paid_employee;
